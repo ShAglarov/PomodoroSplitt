60ef779d43c416cca354e6f9374f492821726937
ShAglarov <shamil.aglarov@gmail.com>
2024-03-03 19:04:51 +0300

    Initial Commit

---
+++ SplitterIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitterIOS
+//
+//  Created by Shamil Aglarov on 03.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
